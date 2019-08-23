43acb2866c712f9ebf2b54a64d8196dc55d8a7a2
noahschwad <noah.schwadron@gmail.com>
2019-08-23 17:48:20 -0600

    fix home links for github pages

--- assets/header2.js
+++ assets/header2.js
@@ -1,13 +1,13 @@
 document.write('\
 \
   <header>\
-    <a href="home.html"><h1 style="float: left;">  NoahDesigns4u.com</h1></a>\
+    <a href="index.html"><h1 style="float: left;">  NoahDesigns4u.com</h1></a>\
     <nav>\
     <img id="menu-icon" src="assets/menu-icon.svg">\
 \
     <ul>\
 \
-    <li><a href="home.html">home</a></li>\
+    <li><a href="index.html">home</a></li>\
     <li><a href="about.html">about</a></li>\
     <li><a href="contact.html">contact</a></li>\
 \
