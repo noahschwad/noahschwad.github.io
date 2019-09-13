1261e13ebca14369b3ec3d3fd65ea9553848317f
noahschwad <noah.schwadron@gmail.com>
2019-09-12 21:28:13 -0600

    bunch'a new content!

--- assets/footer.js
+++ assets/footer.js
@@ -2,10 +2,10 @@ document.write('\
 \
   </div>\
   <div class="foot">\
-    <a href="#" class="fa fa-instagram"></a>\
-    <a href="#" class="fa fa-twitter"></a>\
-    <a href="#" class="fa fa-linkedin"></a>\
-    <a href="#" class="fa fa-youtube"></a>\
+    <a href="https://www.instagram.com/bran_nu_boy/" class="fa fa-2x fa-instagram"></a>\
+    <a href="https://twitter.com/spokenturd" class="fa fa-2x fa-twitter"></a>\
+    <a href="https://www.linkedin.com/in/nschwadron/" class="fa fa-2x fa-linkedin"></a>\
+    <a href="https://www.youtube.com/channel/UCM9ACT27wmlUFTVwtkGX5_A" class="fa fa-2x fa-youtube"></a>\
   </div>\
 \
 ');
