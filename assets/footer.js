1712e424aa3285ff5b5b8b044678f8398fdde98c
noahschwad <noah.schwadron@gmail.com>
2020-01-07 22:21:42 -0700

    new home thumbnails!

--- assets/footer.js
+++ assets/footer.js
@@ -3,9 +3,11 @@ document.write('\
   </div>\
   <div class="foot">\
     <a href="https://www.instagram.com/bran_nu_boy/" class="fa fa-2x fa-instagram"></a>\
-    <a href="https://twitter.com/spokenturd" class="fa fa-2x fa-twitter"></a>\
     <a href="https://www.linkedin.com/in/nschwadron/" class="fa fa-2x fa-linkedin"></a>\
-    <a href="https://www.youtube.com/channel/UCM9ACT27wmlUFTVwtkGX5_A" class="fa fa-2x fa-youtube"></a>\
   </div>\
 \
 ');
+
+//    <a href="https://twitter.com/spokenturd" class="fa fa-2x fa-twitter"></a>\
+
+//    <a href="https://www.youtube.com/channel/UCM9ACT27wmlUFTVwtkGX5_A" class="fa fa-2x fa-youtube"></a>\
