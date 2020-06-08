c12acd5e6c4ca015c7dd4962abc26177e3579186
noahschwad <noah.schwadron@gmail.com>
2020-06-08 15:29:37 -0600

    added mobile nav animation

--- assets/js/general.js
+++ assets/js/general.js
@@ -5,34 +5,38 @@ function hideMenu(event) {
 
   var menu = document.getElementsByTagName('ul')[0]
 
-
-  // console.log(event.target.tagName);
-
-
   if (menu.classList.contains("active")
       && event.target.id != "menu-icon"
       && event.target.id != "navList"
       && event.target.tagName != "LI" ) {
     menu.classList.toggle("active");
     removeEventListener("click", hideMenu);
-    // console.log('menu hidden');
 
   }
 
 }
 
 
-function menuButton() {
+async function menuButton() {
 
   var menu = document.getElementsByTagName('ul')[0]
 
   if (menu.classList.contains("active")) {
     menu.classList.toggle("active");
+
     // console.log('deactivated');
     removeEventListener("click", hideMenu);
+
+    await sleep(300);
+
+    menu.style.display = "none";
+
   }
 
   else {
+    menu.style.display = "flex";
+    await sleep(50);
+    console.log("flex");
     menu.classList.toggle("active");
     // console.log('activated');
     addEventListener("click", hideMenu);
@@ -40,12 +44,6 @@ function menuButton() {
 }
 
 
-// window.onload = function() {
-//   var hidediv = document.getElementsByTagName("ul")[0];
-//   document.onclick = function(div) {
-//     if(div.target.tag !== 'ul' && hide.classList.contains("active")) {
-//       hidediv.classList.toggle("active");
-//       console.log("weird code fired")
-//     }
-//   };
-// };
+function sleep(ms) {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
