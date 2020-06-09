0009ea4dbba62e49a10410b9804d13a7f93e9ed6
noahschwad <noah.schwadron@gmail.com>
2020-06-08 18:28:34 -0600

    improved header, footer, posters

--- assets/js/headerAnimation.js
+++ assets/js/headerAnimation.js
@@ -8,18 +8,20 @@ scrollFunction();
 window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
-  if (document.body.scrollTop > 30 || document.documentElement.scrollTop > 30) {
+  if (document.body.scrollTop > -10 || document.documentElement.scrollTop > -10) { // the -10 values used to be 30. also need to turn transitions back on for a few styles to have old functionality
 
     document.getElementById("headerBox").classList.add("fullHeddrBox");
+    document.getElementsByTagName("header")[0].classList.add("fullHeader");
     document.getElementsByTagName("header")[0].style.top = "0px";
     document.getElementsByTagName("header")[0].style.height = "60px";
-    document.getElementsByTagName("ul")[0].style.right = "-5px";
+    document.getElementsByTagName("ul")[0].style.right = "-1px";
 
 
 
   } else {
 
     document.getElementById("headerBox").classList.remove("fullHeddrBox");
+    document.getElementsByTagName("header")[0].classList.remove("fullHeader");
     document.getElementsByTagName("header")[0].style.top = "25px";
     document.getElementsByTagName("header")[0].style.height = "70px";
     document.getElementsByTagName("ul")[0].style.right = "13px";
